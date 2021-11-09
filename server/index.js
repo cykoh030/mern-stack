555bd0e9293c3afe36d547893697e29e50afb1e6
cykoh030 <jeffkcy32@gmail.com>
2021-11-10 02:48:15 +0800

    register & login local mongodb

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +21,7 @@ mongoose.connect("mongodb://localhost:27017/full-mern-stack-video");
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
   try {
-    const user = await User.create({
+    await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
@@ -32,7 +33,7 @@ app.post("/api/register", async (req, res) => {
 });
 
 app.post("/api/login", async (req, res) => {
-  await User.findOne({
+  const user = await User.findOne({
     email: req.body.email,
     password: req.body.password,
   });
